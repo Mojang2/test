2d81620aa9d6456920ce0625d1780ee14bd95064
Mojang2 <63506753+Mojang2@users.noreply.github.com>
2020-04-11 11:00:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ var preset = function(){
                     
                     	background(215, 151, 66);
                     
-                    	/*for(let i = 0; i < 7; i++){
+                    	for(let i = 0; i < 7; i++){
                     		for(let j = 0; j < 7; j++){
                     			cubes.push([i, 10 - Math.abs(i) - Math.abs(j), j, 1, 1, 1, color(255, i * 30, j * 30), 0, 0, 0, "block", "collisionAdjust"])
                     		}
@@ -163,7 +163,7 @@ var preset = function(){
                     	
                     	for (let i = 0; i < cubes.length; i ++) {
                     	    Object3D.Cuboid(cubes[i][0], cubes[i][1], cubes[i][2], cubes[i][3], cubes[i][4], cubes[i][5], cubes[i][6], cubes[i][7], cubes[i][8], cubes[i][9], cubes[i][10], cubes[i][11], cubes[i][12]);
-                    	}*/
+                    	}
                     	
                     	
                     	/*Object3D.Cuboid(
